61304335ce0f96fca35a4dccf4f3ff33145586f3
Vlemrc <lemerciervictor@gmail.com>
2023-11-21 14:38:24 +0100

    finitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-        extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#202022",
+      },
+      borderRadius: {
+        '4xl': '2rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+      },
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '6rem',
+      }
     },
-    plugins: [],
-    darkMode: "class",
-};
+  },
+  plugins: [],
+  darkMode: "class",
+}
